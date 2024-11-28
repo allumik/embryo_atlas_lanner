a891b2178a6df7df8e4497b71880bca8318ef887
allumik <alvinmeltsov@gmail.com>
2024-11-28 14:30:41 +0100

    rewrote the loom builder to use loompy concat for more ram efficient concatenation

--- download_datasets.py
+++ download_datasets.py
@@ -12,15 +12,16 @@
 import gzip as gz
 import pandas as pd
 import loompy as lm
+import scanpy as sc
 import numpy as np
 import requests
+import os
 from io import BytesIO
 from io import StringIO
 from pathlib import Path
 from os import getenv
 from dotenv import load_dotenv
 from functools import reduce
-from multiprocessing import Pool
 
 load_dotenv()
 
@@ -30,8 +31,23 @@ raw_folder = getenv("RAW_DATA_FOLDER")
 ## ensure that the working data directory is created
 Path(data_folder).expanduser().mkdir(exist_ok=True)
 
-## get ensemble attributes for trying out conversion
-ens_attributes = ["ensembl_gene_id","ensembl_gene_id_version","external_gene_name","external_gene_source","external_synonym","gene_biotype","arrayexpress","genecards","hgnc_id","mirbase_id","entrezgene_id","refseq_mrna","description","start_position","end_position","chromosome_name"]
+def replace_non_ascii(s, replacement='*'):
+  if isinstance(s, str):
+    return ''.join(c if ord(c) < 128 else replacement for c in s)
+  return s # Return as is if not a string
+
+# quick loom file overview function
+def glimpse_loom(loom_path) -> None:
+  with lm.connect(loom_path) as loom_f:
+    print("shape:", loom_f.shape)
+    print("attrs:", list(loom_f.attrs))
+    print("layers:", list(loom_f.layers))
+    print("cols:", list(loom_f.ca))
+    print("rows:", list(loom_f.ra))
+
+
+# %% get ensemble attributes for id conversion
+ens_attributes = ["ensembl_gene_id","ensembl_gene_id_version","external_gene_name","external_gene_source","external_synonym","gene_biotype","arrayexpress","genecards","hgnc_symbol","mirbase_id","entrezgene_id","refseq_mrna","description","start_position","end_position","chromosome_name"]
 
 # download the annotation
 annot_f = Path(raw_folder) / "annot.parquet"
@@ -44,13 +60,49 @@ else:
         sc.queries.biomart_annotations(
           "hsapiens",
           np.concatenate([["ensembl_gene_id_version"], [ens_attr]]),
-          host="www.ensembl.org",
+          host="www.ensembl.org", # useast.ensembl.org
         ).set_index("ensembl_gene_id_version")
         for ens_attr in ens_attributes # np.array_split(ens_attributes, 8)
       ]
   )
   annot.drop(columns=["Gene stable ID version.1"]).infer_objects().to_parquet(annot_f)
 
+# format gene-id annotatoin for row-level annotation
+annot = (
+  annot
+  [["external_gene_name", "ensembl_gene_id", "hgnc_symbol", "start_position", "end_position", "chromosome_name"]]
+  .dropna(subset=["external_gene_name", "hgnc_symbol"])
+  .drop_duplicates(subset=["external_gene_name", "hgnc_symbol"])
+  .rename(columns={"ensembl_gene_id": "Accession", "external_gene_name": "Gene"}) # the loompy convention
+  .set_index("Gene")
+)
+
+
+
+# %% get the table from the website https://petropoulos-lanner-labs.clintec.ki.se/dataset.download.html
+print("Downloading the table from the website")
+url_cols = ["metadata", "counts"]
+html_table = pd.merge( # read it two times, one with links and other without -> easier to format :)
+  left = pd.read_html(
+    "https://petropoulos-lanner-labs.clintec.ki.se/dataset.download.html",
+    extract_links=None
+  )[0].drop(columns=url_cols),
+  right = pd.read_html(
+    "https://petropoulos-lanner-labs.clintec.ki.se/dataset.download.html",
+    extract_links="body"
+  )[0][url_cols],
+  left_index=True, right_index=True
+)
+
+# format the table
+for col in url_cols:
+  html_table[[col, col + "_url"]] = pd.DataFrame(html_table[col].to_list(), index=html_table.index)
+  html_table[col + "_url"] = "https://petropoulos-lanner-labs.clintec.ki.se/" + html_table[col + "_url"].astype(str)
+
+
+
+# %% load the stuff
+print("Trying to download the files into $RAW_DATA_FOLDER")
 # create a bytes stream with the gzip decompression and save the count matrices into a list
 # save it also into the specified path as a parquet file and return the filepath
 def dl_gz(input_url, out_path, overwrite=False):
@@ -71,43 +123,17 @@ def dl_gz(input_url, out_path, overwrite=False):
     print("No operation needed, returning the filename:", outfile)
   return outfile
 
-
-# %% get the table from the website https://petropoulos-lanner-labs.clintec.ki.se/dataset.download.html
-url_cols = ["metadata", "counts"]
-html_table = pd.merge( # read it two times, one with links and other without -> easier to format :)
-  left = pd.read_html(
-    "https://petropoulos-lanner-labs.clintec.ki.se/dataset.download.html",
-    extract_links=None
-  )[0].drop(columns=url_cols),
-  right = pd.read_html(
-    "https://petropoulos-lanner-labs.clintec.ki.se/dataset.download.html",
-    extract_links="body"
-  )[0][url_cols],
-  left_index=True, right_index=True
-)
-
-
-# %% format the table
-for col in url_cols:
-  html_table[[col, col + "_url"]] = pd.DataFrame(html_table[col].to_list(), index=html_table.index)
-  html_table[col + "_url"] = "https://petropoulos-lanner-labs.clintec.ki.se/" + html_table[col + "_url"].astype(str)
-
-# %% load the stuff
-
-# load and join them all into a table with multiprocessing, 
-# split the list of datasets into pieces and, `reduce` allocates too much memory
-with Pool(processes=4) as pool:
-  file_list = pool.starmap(
-    dl_gz,
-    [(url, Path(raw_folder))
-      for url in html_table.counts_url.to_list()]
-  )
+# load and join them all into a table with multiprocessing
+# or do it linearly bc there seems to be some issue with multitasking 
+file_list = []
+for url in html_table.counts_url.to_list():
+    file_list.append(dl_gz(url, Path(data_folder)))  
 html_table["local_file_loc"] = file_list
-# dl_gz(html_table.counts_url[11], Path(raw_folder))
-# Path(Path(html_table.counts_url[11]).stem).stem
+
 
 
 # %% format phenotype per cell id
+print("Downloading and reformatting the phenotype data")
 # get the cell-id to dataset annot
 comb_cid = pd.concat([
   pd.read_parquet(row.local_file_loc)
@@ -134,11 +160,13 @@ comb_pheno = (
   .reset_index()
   .rename(columns={"cell":"CellID"})
   .set_index("CellID")
+  .map(replace_non_ascii)
 )
 del comb_cid
 
 
-# %% load the downloaded datasets, combine them into one based on the geneset included
+
+# %% some dataset filtering for downstream tasks
 
 # annotate datasets that are weird or do not fit our expectations
 weird_data = [
@@ -163,67 +191,72 @@ probe_id_data = [ # other datasets that have probe id's
   "E-MTAB-10581","GSE134571","GSE171820",
   "PRJCA017779"
 ]
-# get the filenames for the datasets
-html_table_rnaseq = html_table[html_table["Accession number"].isin(whole_rna_data)].reset_index()
-html_table_probes = html_table[html_table["Accession number"].isin(probe_id_data)].reset_index()
+# filter out those datasets with those weird data things
+html_table = html_table[
+  ~html_table["Accession number"]
+  .isin(weird_data) # + whole_rna_data
+  ].reset_index(drop=True)
 
 
-# %% combine the datasets now
-whole_comb = reduce(
-  lambda l,r: pd.merge(l, r, how="outer", on="Gene"), [
-    pd.read_parquet(f_path)
-      for f_path in html_table.query("`Accession number` in @whole_rna_data").local_file_loc
-    ]
-  ).convert_dtypes()
 
-# write the larger table out to parquet just to save time on reduce every run
-tmp_writeout=Path(data_folder) / "tmp_combined_writeout.parquet"
-if tmp_writeout.exists():
-  probe_comb = pd.read_parquet(tmp_writeout)
-else:
-  probe_comb = reduce(
-    lambda l,r: pd.merge(l, r, how="outer", on="Gene"), [
-      pd.read_parquet(f_path)
-        for f_path in html_table.query("`Accession number` in @probe_id_data").local_file_loc
-      ]
-    ).convert_dtypes()
-  probe_comb.to_parquet(tmp_writeout)
+# %% get the unique geneids from the datasets for the row attributes
+gene_ids = np.unique(np.concatenate([
+  pd.read_parquet(f_path, columns=["Gene"]).index.to_numpy()
+    for f_path in html_table.local_file_loc
+]).ravel())
 
-
-# %% Create an loom objects from the tables and save it
-# format gene-id annotatoin for row-level annotation
-annot = (
-  annot
-  [["external_gene_name", "ensembl_gene_id", "start_position", "end_position", "chromosome_name"]]
-  .dropna(subset=["external_gene_name"])
-  .drop_duplicates()
-  .set_index("external_gene_name")
-  .rename(columns={"ensembl_gene_id": "Accession"}) # the loompy convention
+# add gene annotation for the existing ids and reorder
+# theres a mixture of id's, so merge for every id type separately
+gene_ids = pd.merge(
+  pd.DataFrame(index=pd.Index(gene_ids, name="Gene")),
+  annot, 
+  how="left", left_index=True, right_index=True
 )
+# # now add from another set of ids for GENCODE IDs
+# gene_ids = pd.merge(
+  # gene_ids,
+  # annot.loc[~np.isin(annot.index, annot.hgnc_symbol)], 
+  # how="left", left_index=True, right_on="hgnc_symbol"
+# )
 
-lm.create(
-  str(Path(data_folder) / "embryo_rnaseq_raw.loom"),
-  whole_comb.to_numpy(dtype=np.integer, na_value=0),
-  whole_comb.index.to_frame().join(annot, how="left").drop_duplicates("Gene").to_dict("list"),
-  comb_pheno.loc[whole_comb.columns].reset_index().to_dict("list")
-)
 
-lm.create(
-  str(Path(data_folder) / "embryo_probeset_raw.loom"),
-  probe_comb.to_numpy(dtype=np.integer, na_value=0),
-  probe_comb.index.to_frame().join(annot, how="left").drop_duplicates("Gene").to_dict("list"),
-  comb_pheno.loc[probe_comb.columns].reset_index().to_dict("list")
-)
+# %% load parquet files and add them to the loom file iteratively 
+# more memory efficient than combining parquets into a large matrix and saving it in one go
+# like a lot more efficient
 
-# and finally create a dataset of inner join between probe and whole datasets
-comb_dat = pd.merge(whole_comb, probe_comb, how="inner", on="Gene")
-lm.create(
-  str(Path(data_folder) / "embryo_raw.loom"),
-  comb_dat.to_numpy(dtype=np.integer, na_value=0),
-  comb_dat.index.to_frame().join(annot, how="left").drop_duplicates("Gene").to_dict("list"),
-  comb_pheno.loc[comb_dat.columns].reset_index().to_dict("list")
+print("Starting to write into the loom files")
+loom_files=[]
+for f_path in html_table.local_file_loc: 
+  print("Writing loom from file:", f_path)
+  count_df = pd.read_parquet(f_path).convert_dtypes()
+  loom_f = str(Path(data_folder) / (Path(f_path.stem).stem + ".loom"))
+  lm.create(
+    filename=loom_f,
+    layers=(
+      count_df
+      [~count_df.index.duplicated(keep="first")] # some Gene id's seem to be duplicated
+      .reindex(gene_ids.index) # realign the data matrix to gene_ids
+      .to_numpy(dtype=np.integer, na_value=0)
+    ),
+    row_attrs=gene_ids.reset_index().to_dict("list"),
+    col_attrs=comb_pheno.loc[count_df.columns].reset_index().to_dict("list")
+  )
+  loom_files.append(loom_f)
+  # and remove the parquet files to save up diskspace
+  os.remove(f_path)
+
+print("Combining and writing out the raw dataset:")
+lm.combine(
+  loom_files, 
+  Path(data_folder).expanduser() / "embryo_lanner_comb_raw.loom", 
+  key="Gene"
 )
+# now remove the loom files to save up on space :)
+for f_path in loom_files: os.remove(f_path)
 
 # testing grounds
+# glimpse_loom(loom_files[2])
 # import anndata as an
 # an.read_loom(Path(data_folder) / "embryo_rnaseq_raw.loom")
+
+# %%
