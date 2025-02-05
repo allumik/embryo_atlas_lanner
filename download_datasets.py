d3aa2a349722612b36411f97a6c6392211efda78
allumik <alvinmeltsov@gmail.com>
2025-02-05 16:31:56 +0100

    Updateed formatting, cleanup some code

--- download_datasets.py
+++ download_datasets.py
@@ -38,7 +38,12 @@ def replace_non_ascii(s, replacement='*'):
   return s # Return as is if not a string
 
 # quick loom file overview function
-def glimpse_loom(loom_path) -> None:
+def glimpse_loom(loom_path:str) -> None:
+  """Get a glimpse of the structure and dimensions of the `loom_path` Loom file.
+
+  Args:
+      loom_path (str): The path to The Great Loominess
+  """
   with lm.connect(loom_path) as loom_f:
     print("shape:", loom_f.shape)
     print("attrs:", list(loom_f.attrs))
@@ -46,7 +51,12 @@ def glimpse_loom(loom_path) -> None:
     print("cols:", list(loom_f.ca))
     print("rows:", list(loom_f.ra))
 
-def filter_zero_genes(loom_file_path):
+def filter_zero_genes(loom_file_path:str):
+  """Remove all the attributes / rows that have in total zero reads from the `loom_file_path` Loom file.
+
+  Args:
+      loom_file_path (str): The path to the loom file.
+  """
   with lm.connect(loom_file_path, mode='r') as ds:
     matrix = ds.layers[''].sparse().tocsr()
     zero_gene_indices = np.where(np.diff(matrix.indptr) == 0)[0]
@@ -62,21 +72,111 @@ def filter_zero_genes(loom_file_path):
   lm.create(str(loom_file_path), vw.layers, vw.ra, vw.ca)
 
 # get the unique geneids from the datasets for the row attributes
-def get_unique_geneids(file_list, annotation):
+def get_unique_geneids(file_list:list, annotation:pd.DataFrame):
+  """Read in all the parquet formatted files in the `file_list`, find the unique common index (aka intersection of index names) and combine it with the `annotation` table from Ensemble biomart.
+
+  Args:
+      file_list (list): List of paths to parquet files. Should have a column named "Gene".
+      annotation (pd.DataFrame): The annotation table from biomart API. Should have a named "Gene" index column.
+
+  Returns:
+      pd.DataFrame: A new table with the annotated gene id's from the `file_list` collection of parquet tables.
+  """
   gene_ids = np.unique(np.concatenate([
     pd.read_parquet(f_path, columns=["Gene"]).index.to_numpy()
       for f_path in file_list
   ]).ravel())
 
   # add gene annotation for the existing ids and reorder
-  # theres a mixture of id's, so merge for every id type separately
   return pd.merge(
     pd.DataFrame(index=pd.Index(gene_ids, name="Gene")),
     annotation,
     how="left", left_index=True, right_index=True
   )
 
+def format_and_loom(
+  file_list:list, 
+  phenotype:pd.DataFrame, 
+  annotation:pd.DataFrame, 
+  output_file:str, 
+  data_output:Path=Path("./data/")
+  ) -> str:
+  """Takes in arguments and generates a loom file, outputting the path to the loom file.
+  This is more memory efficient than combining parquets into a large matrix and saving it in one go.
 
+  Args:
+      file_list (list): List of parquet files to load in.
+      phenotype (pd.DataFrame): Table containing the column attributes (features) of the matrix.
+      annotation (pd.DataFrame): Annotation for the gene id's in the row attributes.
+      output_file (str): The Loom file name to create.
+      data_output (Path, optional): The path to the folder for the output file. Defaults to Path("./data/"). Pretty pointless, should be included in output_file...
+
+  Returns:
+      str: The path to the generated Loom file.
+  """
+
+  gene_ids = get_unique_geneids(file_list, annotation)
+  loom_files = []
+  for f_path in file_list: 
+    count_df = pd.read_parquet(f_path).convert_dtypes()
+    loom_f = str(data_folder / (Path(f_path.stem).stem + ".loom"))
+    lm.create(
+      filename=loom_f,
+      layers=(
+        count_df
+        [~count_df.index.duplicated(keep="first")] # some Gene id's seem to be duplicated
+        .reindex(gene_ids.index) # realign the data matrix to gene_ids
+        .to_numpy(dtype=np.integer, na_value=0)
+      ),
+      row_attrs=gene_ids.reset_index().to_dict("list"),
+      col_attrs=phenotype.loc[count_df.columns].reset_index().to_dict("list")
+    )
+    loom_files.append(loom_f)
+
+  # combine the single loom files
+  output_loom = data_output / output_file
+  lm.combine(
+    loom_files,
+    output_loom
+    # key="Gene" # dont need it as everything is aligned already. also breaks for some reason
+  )
+
+  # and then remove the 0 rows to make the dataset more compact (TODO: but what about dropouts?)
+  filter_zero_genes(output_loom)
+
+  # finally, remove the intermediary loom files
+  for f_path in loom_files: os.remove(f_path)
+
+  # and return the output file location as string
+  return str(output_loom)
+
+def dl_gz(input_url:str, out_path, overwrite=False) -> str: 
+  """Create a bytes stream from the specified URL with the gzip decompression. Then saves it into the specified `out_path` as a parquet file and returns the filepath.
+
+  Args:
+      input_url (str): The URL from where you wish to download the gzipped table.
+      out_path: Path or str of the output file folder. The file name is created from the input url.
+      overwrite (bool, optional): Do you wish to overwrite the existing file at location? Defaults to False.
+
+  Returns:
+      str: The created Parquet file location.
+  """
+  outfile = Path(out_path) / (Path(input_url).stem + ".parquet")
+
+  ## check if the file's already there
+  if overwrite or (not outfile.exists()):
+    ## read the file in with gunzip
+    print("Downloading table from:", input_url)
+    response = requests.get(input_url)
+    with gz.open(filename=BytesIO(response.content), mode="rb") as gz_file:
+      outtable = pd.read_csv(gz_file).infer_objects().set_index("Gene")
+
+    ## write the file out to file
+    print("Writing it to:", outfile)
+    outtable.to_parquet(outfile)
+  else:
+    print("No operation needed, returning the filename:", outfile)
+  return outfile
 
 
 # %% get ensemble attributes for id conversion
@@ -145,25 +245,6 @@ html_table.loc[33, "Publication"] = "Ai et al., 2023 - assembloid"
 
 # %% load the stuff
 print("Trying to download the files into $RAW_DATA_FOLDER")
-# create a bytes stream with the gzip decompression and save the count matrices into a list
-# save it also into the specified path as a parquet file and return the filepath
-def dl_gz(input_url, out_path, overwrite=False):
-  outfile = Path(out_path) / (Path(input_url).stem + ".parquet")
-
-  ## check if the file's already there
-  if overwrite or (not outfile.exists()):
-    ## read the file in with gunzip
-    print("Downloading table from:", input_url)
-    response = requests.get(input_url)
-    with gz.open(filename=BytesIO(response.content), mode="rb") as gz_file:
-      outtable = pd.read_csv(gz_file).infer_objects().set_index("Gene")
-
-    ## write the file out to file
-    print("Writing it to:", outfile)
-    outtable.to_parquet(outfile)
-  else:
-    print("No operation needed, returning the filename:", outfile)
-  return outfile
 
 # load and join them all into a table with multiprocessing
 # or do it linearly bc there seems to be some issue with multitasking 
@@ -176,7 +257,7 @@ with Pool(processes=4) as pool:
 html_table["local_file_loc"] = file_list # assume the dl order was the same
 
 
-# %% Now add the modified suppl. table 9
+# %% Now add the modified suppl. table 9 from the article
 # first read it in
 suppl_datasets = pd.read_csv(data_folder / "41592_2024_2493_MOESM11_ESM_modified.csv")
 
@@ -229,48 +310,6 @@ del comb_cid
 
 
 
-# %% load parquet files and add them to the loom file iteratively 
-
-# this is more memory efficient than combining parquets into a large matrix and saving it in one go
-# ... like a lot more RAM efficient
-def format_and_loom(file_list:list, phenotype:pd.DataFrame, annotation:pd.DataFrame, output_file:str, data_output=Path("./data/")) -> str:
-  gene_ids = get_unique_geneids(file_list, annotation)
-  loom_files = []
-  for f_path in file_list: 
-    count_df = pd.read_parquet(f_path).convert_dtypes()
-    loom_f = str(data_folder / (Path(f_path.stem).stem + ".loom"))
-    lm.create(
-      filename=loom_f,
-      layers=(
-        count_df
-        [~count_df.index.duplicated(keep="first")] # some Gene id's seem to be duplicated
-        .reindex(gene_ids.index) # realign the data matrix to gene_ids
-        .to_numpy(dtype=np.integer, na_value=0)
-      ),
-      row_attrs=gene_ids.reset_index().to_dict("list"),
-      col_attrs=phenotype.loc[count_df.columns].reset_index().to_dict("list")
-    )
-    loom_files.append(loom_f)
-
-  # and now combine the single loom files
-  output_loom = data_output / output_file
-  lm.combine(
-    loom_files,
-    output_loom
-    # key="Gene" # dont need it as everything is aligned already. also breaks for some reason
-  )
-
-  # and then remove the 0 rows to make the dataset more compact (TODO: but what about dropouts?)
-  filter_zero_genes(output_loom)
-
-  # finally, remove the intermediary loom files
-  for f_path in loom_files: os.remove(f_path)
-
-  # and return the output file location as string
-  return str(output_loom)
-
-
-
 # %% Now start writing out the dataasets into loom
 ## first load and combine the "core" set
 comb_loom_list = [
@@ -300,19 +339,10 @@ for f_path in html_table.local_file_loc: os.remove(f_path)
 
 
 # %% testing grounds
-# glimpse_loom(loom_files[2])
-glimpse_loom(data_folder / "comb_core_raw.loom")
-glimpse_loom(data_folder / "comb_g1_raw.loom")
+# glimpse_loom(data_folder / "comb_core_raw.loom")
+# glimpse_loom(data_folder / "comb_g1_raw.loom")
 # import anndata as an
 # sc_dat = an.read_loom(data_folder / "comb_core_raw.loom")
 # print(sc_dat)
 # print(sc_dat.var.index)
 # sc_dat.write_h5ad(data_folder / "embryo_lanner_comb_raw.h5ad")
-
-# genes_ = []
-# for f_path in loom_files:
-  # # glimpse_loom(f_path)
-  # with lm.connect(f_path) as loom_f:
-    # print("attrs:", np.unique(loom_f.ra["Gene"], return_counts=True))
-
-# %%
